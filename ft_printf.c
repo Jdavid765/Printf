192622121951b35d0d5ebee9831d8bdead17b15f
Jdavid765 <david.canoduran@hotmail.com>
2025-10-18 18:12:23 +0200

    faire le main du printf en cours

--- ft_printf.c
+++ ft_printf.c
@@ -6,30 +6,50 @@
 /*   By: david <david@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 14:58:49 by yourlogin         #+#    #+#             */
-/*   Updated: 2025/10/18 16:27:15 by david            ###   ########.fr       */
+/*   Updated: 2025/10/18 18:01:24 by david            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
-int	ft_printf(const char *s, ...)
+int	ft_selectf(char tmp, va_list current)
 {
-    va_list args;
-    va_start(args, s);
+	int	count;
+
+	count = 0;
+	if (tmp == 's')
+		count += ft_printf_str(va_arg(current, char *));
+	else if (tmp == 'c')
+		count += ft_printf_char(va_arg(current, int));
+	else
+		return (1);
+	return (0);
+}
+
+int	ft_printf(const char *format, ...)
+{
+    va_list	current;
 	char	tmp;
+	int	count;
 
-	
-    for (int i = 0; *s; ++i) {
-		tmp = va_args(arg)
-    }
+	count = 0;
+	va_start	(current, format);
+	while(*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			tmp = *format;
+			count += ft_selectf(tmp, current);	
+		}	
+	}
+	va_end(current);
 
-    va_end(args);
-
-    return total;
+    return (count);
 }
 
 int main() {
-    int resultat = somme(6, 10, 20, 30, 40, 50, 50); // total = 150
-    printf("Résultat = %d\n", resultat);
+    const char s[] = "bonjour pauk";
+    ft_printf("Résultat = %s\n", s);
     return 0;
 }
